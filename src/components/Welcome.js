6222b0dd801b21f95054531e5684980f005435f7
giuliacajati <giuliacajati@gmail.com>
2020-12-16 22:43:46 -0500

    front end skeleton

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = () => {
+
+    return (
+        <div>
+            Welcome
+        </div>
+    )
+
+}
+
+export default Welcome
---
+++ src/containers/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+
+    return (
+        <div>
+            NavBar (AboutMe Projects)
+        </div>
+    )
+
+}
+
+export default NavBar
