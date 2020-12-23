608859eb209c5e92f78246818ab17b4821e83a05
giuliacajati <giuliacajati@gmail.com>
2020-12-22 22:19:32 -0500

    setting up containers

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+    container:{
+        backgroundColor: '#cfe8fc', 
+        height: '80vh',
+    },
+    welcome:{
+       marginLeft: '80vh',
+      // padding: '0px'
+    } 
+  });
 
 const Projects = () => {
+    const classes = useStyles();
+
 
     return (
-        <div>
-            Projects Gallery
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+                <Container maxWidth="xl" className={classes.container}>
+                <Container className={classes.welcome}>
+                PROJECTS
+
+                </Container>  
+            </Container>
+        </React.Fragment>
+
     )
 
 }
