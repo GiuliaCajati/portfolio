ccb48f1a1e6a3c544ecca26c482f0bc3b081e625
giuliacajati <giuliacajati@gmail.com>
2020-12-28 17:11:39 -0500

    added material ui grid for projects

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,32 +1,85 @@
+// import React from 'react';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Container from '@material-ui/core/Container';
+
+// const useStyles = makeStyles({
+//     container:{
+//         backgroundColor: '#cfe8fc', 
+//         height: '80vh',
+//     },
+//   });
+
+// const Projects = () => {
+//     const classes = useStyles();
+
+
+//     return (
+//         <React.Fragment>
+//             <CssBaseline />
+//                 <Container maxWidth="xl" className={classes.container}>
+//                 <Container className={classes.welcome}>
+//                 Projects
+
+//                 </Container>  
+//             </Container>
+//         </React.Fragment>
+
+//     )
+
+// }
+
+// export default Projects
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 
-const useStyles = makeStyles({
-    container:{
-        backgroundColor: '#cfe8fc', 
-        height: '80vh',
-    },
-  });
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-const Projects = () => {
-    const classes = useStyles();
+export default function NestedGrid() {
+  const classes = useStyles();
 
-
+  function FormRow() {
     return (
-        <React.Fragment>
-            <CssBaseline />
-                <Container maxWidth="xl" className={classes.container}>
-                <Container className={classes.welcome}>
-                Projects
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>Project</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>items</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 
-                </Container>  
-            </Container>
-        </React.Fragment>
-
-    )
-
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-export default Projects
