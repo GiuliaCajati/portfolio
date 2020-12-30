23c8cedeec2f758c075f4a31b1076fa9ad6bd6d8
giuliacajati <giuliacajati@gmail.com>
2020-12-30 10:50:30 -0500

    images/formatting added to projects

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -40,12 +40,19 @@ import Container from '@material-ui/core/Container';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginLeft: '2%'
   },
   paper: {
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  image: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  }
 }));
 
 export default function NestedGrid() {
@@ -55,24 +62,25 @@ export default function NestedGrid() {
     return (
       <React.Fragment>
         <Grid item xs={4}>
-          <Paper className={classes.paper}>Project</Paper>
+          <Paper className={classes.paper}><img className={classes.image} alt="hotel toolkit" src="https://i.imgur.com/cBy9Pa2.png"/></Paper>
+          {/* login "https://i.imgur.com/pEMjPFq.png" */}
         </Grid>
         <Grid item xs={4}>
-          <Paper className={classes.paper}>item</Paper>
+          <Paper className={classes.paper}><img className={classes.image} alt="bon appetit" src="https://i.imgur.com/vrGfOmV.png"/></Paper>
+          {/* "https://i.imgur.com/297AXe1.png" */}
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>items</Paper>
         </Grid>
+    
       </React.Fragment>
     );
   }
 
   return (
     <div className={classes.root}>
+        <b>PROJECTS</b>
       <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
         <Grid container item xs={12} spacing={3}>
           <FormRow />
         </Grid>
